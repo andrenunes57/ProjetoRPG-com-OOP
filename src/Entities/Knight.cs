63fa189a46e17a1fea1ac9d11f8940eec01c4242
André Nunes de Lima <andre.nunes@live.com>
2021-06-30 19:47:10 -0300

    retirando comentários do código

--- src/Entities/Knight.cs
+++ src/Entities/Knight.cs
@@ -45,12 +45,3 @@ namespace ProjetoRPG.src.Entities
         }
     }
 }
-
-        //Implementar os métodos de ataques para cada classe, diminuindo o HP se aplicável
-        //Implementar os métodos de tomar dano, dimunuindo o HP
-        //Implementar no ToString o status do HP e MP, incluindo mensagem de "Personagem morreu", caso o HP chegue a zero
-        //Implementar o tostring da cura/dano em character
-        //Gravar video do console e editar com som de batalha
-        //Incluir Delay para rodar os turnos! e causar tensão!
-        //Dificuldade em implemnetar interface, classe abstrata
-        //Aprendendo se divertinho, realizar sonho de desenvolver games
